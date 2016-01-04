cddd6a6021e00886ce42e3669baccf09535e2fca
jimmyandhurry <y.syao@163.com>
2016-01-04 15:12:05 +0800

    Initial Commit

---
+++ swiftpostingsTests/swiftpostingsTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftpostingsTests.swift
+//  swiftpostingsTests
+//
+//  Created by 易诗尧 on 16/1/4.
+//  Copyright © 2016年 yishiyao. All rights reserved.
+//
+
+import XCTest
+@testable import swiftpostings
+
+class swiftpostingsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
