6ae28705dcca41181151495f8414cbc69a1abfb5
jimmyandhurry <y.syao@163.com>
2016-01-08 16:48:17 +0800

    增加列表编辑、删除功能。

--- swiftpostings/UITableViewApsViewController.swift
+++ swiftpostings/UITableViewApsViewController.swift
@@ -9,28 +9,71 @@
 import UIKit
 
 class UITableViewApsViewController: UITableViewController {
+    enum UIBarButtonStatus {
+        case EDITING, NONE
+    }
+    
     let cellIdentifier = "imageTableCell"
     let cellNibName = "ImageTableCell"
+    var cell: ImageTableViewCell?
+    var cellCount: Int = 50
+
+    var editButton: UIBarButtonItem {
+        get {
+            return UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("edit"))
+        }
+    }
+    var backButton: UIBarButtonItem {
+        get{
+            return UIBarButtonItem(title: "Close", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("close"))
+        }
+    }
+    var doneButton: UIBarButtonItem {
+        get {
+            return UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("done"))
+        }
+    }
+    
+    var isDisplayEditingBar: Bool? {
+        set {
+            if let newValue1 = newValue {
+                if newValue1 {
+                    self.navigationItem.leftBarButtonItem = nil
+                    self.navigationItem.rightBarButtonItem = doneButton
+                } else {
+                    self.navigationItem.rightBarButtonItem = editButton
+                    self.navigationItem.leftBarButtonItem = backButton
+                }
+            }
+        }
+        get {
+            return self.isDisplayEditingBar
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let editButton: UIBarButtonItem = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("edit"))
-        self.navigationItem.rightBarButtonItem = editButton
-        let backButton: UIBarButtonItem = UIBarButtonItem(title: "Close", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("close"))
-        self.navigationItem.leftBarButtonItem = backButton
+        isDisplayEditingBar = false
     }
     
     func edit() -> Void {
-        print("Press Edit..")
+        isDisplayEditingBar = true
+        self.tableView.setEditing(true, animated: true)
     }
     
     func close() -> Void {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    func done() -> Void {
+         isDisplayEditingBar = false
+        self.tableView.setEditing(false, animated: true)
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell: ImageTableViewCell? = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? ImageTableViewCell
+        cell = self.tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? ImageTableViewCell
         if (cell == nil) {
             let nib = NSBundle.mainBundle().loadNibNamed(cellNibName, owner: self, options: nil)
             cell = nib[0] as? ImageTableViewCell
@@ -45,11 +88,38 @@ class UITableViewApsViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return cellCount
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
+    }
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if(cell != nil) {
+            let size: CGSize = (cell?.contentView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize))!
+            return (size.height + 1)
+        }
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 78
+    }
+    
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.Delete
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        switch editingStyle {
+        case UITableViewCellEditingStyle.Delete:
+            cellCount -= 1
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+            break
+        default:
+            return
+        }
     }
 
     /*
